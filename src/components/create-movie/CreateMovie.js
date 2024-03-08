5c43056aa75771feaf5a9edefd2d0a2e6563adf1
azykdev <azyk9114@gmail.com>
2024-03-08 20:21:17 +0500

    #5 - Worked in CreateMovie component

--- src/components/create-movie/CreateMovie.js
+++ src/components/create-movie/CreateMovie.js
@@ -1,27 +1,60 @@
 import "./CreateMovie.css";
+import { Component } from "react";
 
-const CreateMovie = () => {
-  return (
-    <div className="create-movie">
-      <h3>Create Movie</h3>
+class CreateMovie extends Component {
+  constructor(props) {
+    super(props);
 
-      <form className="mt-3">
-        <div className="row">
-          <div className="col-md-6">
-            <input type="text" placeholder="Movie Title" className="" />
+    this.state = {
+      title: "aaa",
+      rating: "",
+    };
+  }
+
+  chageHandler = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  };
+  render() {
+    const { title, rating } = this.state;
+
+    return (
+      <div className="create-movie">
+        <h3>Create Movie</h3>
+
+        <form className="mt-3">
+          <div className="row">
+            <div className="col-md-6">
+              <input
+                onChange={this.chageHandler}
+                type="text"
+                placeholder="Movie Title"
+                className=""
+                value={title}
+                name="title"
+              />
+            </div>
+            <div className="col-md-3">
+              <input
+                onChange={this.chageHandler}
+                type="text"
+                placeholder="Movie Rating"
+                className=""
+                value={rating}
+                name="rating"
+              />
+            </div>
+            <div className="col-md-3">
+              <button className="rounded" type="submit">
+                Create
+              </button>
+            </div>
           </div>
-          <div className="col-md-3">
-            <input type="text" placeholder="Movie Year" />
-          </div>
-          <div className="col-md-3">
-            <button className="rounded" type="submit">
-              Create
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
-};
+        </form>
+      </div>
+    );
+  }
+}
 
 export default CreateMovie;
--- src/components/movie-list-item/MovieListItem.js
+++ src/components/movie-list-item/MovieListItem.js
@@ -1,22 +1,59 @@
 import "./MovieListItem.css";
+import { Component } from "react";
 
-const MovieListItem = (movie) => {
-  return (
-    <div
-      className={`movie-list-item d-flex justify-content-between align-items-center ${movie.cookie && "cookie"} ${movie.star && "star"}`}
-    >
-      <h6 className="m-0">{movie.title}</h6>
+class MovieListItem extends Component {
+  constructor(props) {
+    super(props);
 
-      <div>
-        <span id="rating" className="me-5 py-1 px-2 rounded text-dark">
-          {movie.rating}
-        </span>
-        <span id="cookie" className="mdi mdi-cookie mx-1"></span>
-        <span id="delete" className="mdi mdi-delete mx-1"></span>
-        <span id="star" className="mdi mdi-star mx-1"></span>
+    this.state = {
+      cookie: false,
+      star: false,
+    };
+  }
+
+  toggleCookie = () => {
+    this.setState(({ cookie }) => ({ cookie: !cookie }));
+  }
+  render() {
+    const { title, rating } = this.props;
+    const { cookie, star } = this.state;
+
+    return (
+      <div
+        className={`movie-list-item d-flex justify-content-between align-items-center ${cookie && "cookie"} ${star && "star"}`}
+      >
+        <h6 className="m-0">{title}</h6>
+  
+        <div>
+          <span id="rating" className="me-5 py-1 px-2 rounded text-dark">
+            {rating}
+          </span>
+          <span onClick={this.toggleCookie} id="cookie" className="mdi mdi-cookie mx-1"></span>
+          <span id="delete" className="mdi mdi-delete mx-1"></span>
+          <span onClick={() => this.setState({ star: !star })} id="star" className="mdi mdi-star mx-1"></span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+// const MovieListItem = (movie) => {
+//   return (
+//     <div
+//       className={`movie-list-item d-flex justify-content-between align-items-center ${movie.cookie && "cookie"} ${movie.star && "star"}`}
+//     >
+//       <h6 className="m-0">{movie.title}</h6>
+
+//       <div>
+//         <span id="rating" className="me-5 py-1 px-2 rounded text-dark">
+//           {movie.rating}
+//         </span>
+//         <span id="cookie" className="mdi mdi-cookie mx-1"></span>
+//         <span id="delete" className="mdi mdi-delete mx-1"></span>
+//         <span id="star" className="mdi mdi-star mx-1"></span>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default MovieListItem;
